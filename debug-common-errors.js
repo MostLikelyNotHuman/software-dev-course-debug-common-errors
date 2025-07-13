63de3a83f56790126dbb25a7689e852ca62943a5
NotHuman <dannypercival14@gmail.com>
2025-07-13 03:55:11 -0500

    fixed three errors

--- debug-common-errors.js
+++ debug-common-errors.js
@@ -24,39 +24,63 @@ Think about which debugging methods you found most useful and how you might appl
 // Description:
 // This program is intended to display a simple prompt in the console but fails to run.
 
-console.log("Welcome to the bootcamp
+//console.log("Welcome to the bootcamp
 
 // What’s Wrong?
+
+//Syntax error - missing a closing quote and parenthesis - correction:
+console.log("Welcome to the bootcamp");
 
 
 // Program B
 // Description:
 // This code attempts to multiply each number in an array by 2 and display the results. However, it crashes at runtime.
 
-let numbers = [2, 4, "eight"];
+//let numbers = [2, 4, "eight"];
+//for (let i = 0; i < numbers.length; i++) {
+//  let doubled = numbers[i] * 2;
+//  console.log(doubled);
+//}
+
+// What’s Wrong?
+
+//Runtime error - code is attempting to multiply the string "eight" by two by converting it into a number, resulting in NaN.
+//correction:
+
+let numbers = [2, 4, 8];
 for (let i = 0; i < numbers.length; i++) {
   let doubled = numbers[i] * 2;
   console.log(doubled);
 }
-
-// What’s Wrong?
-
 
 
 // Program C (Logic Error)
 // Description:
 // This snippet of code is supposed to check if a given number is prime (i.e., divisible only by 1 and itself). However, it incorrectly marks some numbers as prime or not prime.
 
+//function isPrime(num) {
+//  if (num < 2) return false;
+//  for (let i = 2; i < num; i++) {
+//   if (num % i === 0) {
+//      return true;  // Supposed to indicate num is NOT prime
+//    }
+//  }
+//  return false; // Supposed to indicate num IS prime
+//}
+
+//console.log(isPrime(7)); // Expected true but gets false
+
+// What’s Wrong?
+
+//Logic error - the return true; and return false; have been mistakenly reversed
+
 function isPrime(num) {
   if (num < 2) return false;
   for (let i = 2; i < num; i++) {
-    if (num % i === 0) {
-      return true;  // Supposed to indicate num is NOT prime
+   if (num % i === 0) {
+      return false;  // Supposed to indicate num is NOT prime
     }
   }
-  return false; // Supposed to indicate num IS prime
+  return true; // Supposed to indicate num IS prime
 }
-
-console.log(isPrime(7)); // Expected true but gets false
-
-// What’s Wrong?
+console.log(isPrime(7));
